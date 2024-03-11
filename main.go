085dcb30b239bcf4f8d4e2e49351f410f3dd529b
17Peeyush <peeyushkushwaha17@gmail.com>
2024-03-11 21:31:29 +0530

    Added multiple times url shortening and removed comments

--- main.go
+++ main.go
@@ -10,11 +10,6 @@ import (
 
 func main() {
 	db.InitDB()
-	// m :=models.Meet{
-	// 	UniqueID: "a1OXylP",
-	// 	Url: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTI4MjFkZDctZDUwMS00MmJhLWFlZTctNzM1N2VlZGI3MGIy%40thread.v2/0?context=%7b%22Tid%22%3a%22e6c83ca8-8322-4c7e-b01c-83474d4c1e19%22%2c%22Oid%22%3a%22e7b39be9-fa48-4c51-944b-32aee32a1e48%22%7d",
-	// }
-	// db.InsertData(m)
 	server := gin.Default()
 
 	// Load HTML templates from the templates directory
